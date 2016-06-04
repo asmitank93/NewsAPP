5e78c34bd44cd4b96f93847144c511513f2bc2ba
Mac <tops1111@Macs-iMac.local>
2016-06-04 11:38:45 +0530

    Initial Commit

---
+++ NewsAPP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewsAPP
+//
+//  Created by Mac on 6/4/16.
+//  Copyright (c) 2016 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewsAPP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewsAPP
+//
+//  Created by Mac on 6/4/16.
+//  Copyright (c) 2016 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
